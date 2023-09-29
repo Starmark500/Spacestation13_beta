920d3c59174c908125b9b96a79b13f8018bdf38d
stepz <step.zip@mail.ru>
2023-09-29 18:04:28 +0300

    29.09.2023

--- main.py
+++ main.py
@@ -1,5 +1,55 @@
 from wrap import sprite,world,sprite_text
 import wrap
+x_wall=100
+y_wall=100
+sprite_id=0
 wrap.add_sprite_dir("my_sprites")
 world.create_world(1000,1000)
-sprite.add("wall",100,100,"wall")
+while True:
+    x_wall=x_wall+30
+    sprite.add("wall", x_wall, 100, "wall")
+    sprite_id=sprite_id+1
+    print(sprite_id)
+    if sprite_id==20:
+        break
+
+sprite_id=0
+while True:
+
+    y_wall=y_wall+30
+    sprite.add("wall", 100, y_wall, "wall")
+    sprite_id=sprite_id+1
+    print(sprite_id)
+    if sprite_id==20:
+        break
+sprite_id=0
+x_wall=730
+y_wall=100
+while True:
+    y_wall=y_wall+30
+    sprite.add("wall", x_wall, y_wall, "wall")
+    sprite_id = sprite_id + 1
+    print(sprite_id)
+    if sprite_id == 20:
+        break
+sprite_id=0
+y_wall=730
+x_wall=100
+while True:
+    x_wall=x_wall+30
+    sprite.add("wall", x_wall, y_wall, "wall")
+    sprite_id = sprite_id + 1
+    print(sprite_id)
+    if sprite_id == 20:
+        break
+
+
+def sozdanie_1_wall(pos_wall,y_wall,x_wall):
+    sprite_id=0
+    while True:
+        pos_wall=pos_wall+30
+        sprite.add("wall", x_wall, y_wall, "wall2")
+        sprite_id = sprite_id + 1
+        if sprite_id == 20:
+            break
+f(x_wall,600,50)
